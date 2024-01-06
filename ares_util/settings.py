9aa39dabb343182ec862904d48b9711844e003df
Marek Procházka <m.prochazka2002@gmail.com>
2024-01-06 10:37:44 +0100

    Sub register call support

--- ares_util/settings.py
+++ ares_util/settings.py
@@ -5,3 +5,20 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 
 COMPANY_ID_LENGTH = 8
 ARES_API_URL = 'https://ares.gov.cz/'
+
+# Identifiers returned by base endpoint (/ekonomicke-subjekty/) mapped to their respective endpoints
+SUB_REGISTER_MAP = {
+    "stavZdrojeVr": "ekonomicke-subjekty-vr/",
+    "stavZdrojeRes": "ekonomicke-subjekty-res/",
+    "stavZdrojeRzp": "ekonomicke-subjekty-rzp/",
+    "stavZdrojeNrpzs": "ekonomicke-subjekty-nrpzs/",
+    "stavZdrojeRpsh": "ekonomicke-subjekty-rpsh/",
+    "stavZdrojeRcns": "ekonomicke-subjekty-rcns/",
+    "stavZdrojeSzr": "ekonomicke-subjekty-szr/",
+    "stavZdrojeDph": "ekonomicke-subjekty-dph/",
+    "stavZdrojeSd": "ekonomicke-subjekty-sd/",
+    "stavZdrojeIr": "ekonomicke-subjekty-ir/",
+    "stavZdrojeCeu": "ekonomicke-subjekty-ceu/",
+    "stavZdrojeRs": "ekonomicke-subjekty-rs/",
+    "stavZdrojeRed": "ekonomicke-subjekty-red/",
+}
